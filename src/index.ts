80b9b475df66b3081c04d249105f108a8b1edfac
Alessandro Kobashigawa <46635611+alefks@users.noreply.github.com>
2024-06-07 12:08:45 -0300

    Feature/add role db config (#1)
    * feat: add seed and migration role
    
    * refactor: server config and environment
    
    * refactor: add a specific file for role-seed.ts
    
    * feat: add env example
    
    * refactor: remove build folder from gitignore
    
    * refactor: update typescript version, change build script, change script
    names of db migrate and db seed, change prisma seed path
    
    * refactor: remove previous role table migration "init" and add a new role table migration "create_role_table"
    
    * refactor: change outDir from "./build" to "./dist"
    
    * refactor: remove seed.ts, split into main-seed.ts and role-seed.ts

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,5 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Running on port: ${PORT}`)
+  console.log(`Running on port: ${PORT}`);
 });
