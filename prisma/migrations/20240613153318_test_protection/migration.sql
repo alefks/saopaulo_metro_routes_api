a43985de46c916e17bda3860c3bb497fbd680a5c
Alessandro Kobashigawa <ale.kobashigawa@outlook.com>
2024-06-13 12:55:34 -0300

    create a test migration

---
+++ prisma/migrations/20240613153318_test_protection/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "TestMigrationProtection" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+
+    CONSTRAINT "TestMigrationProtection_pkey" PRIMARY KEY ("id")
+);
